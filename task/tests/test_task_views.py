8b64b8f15c064c1a993f9adc0712413e0ab2cee6
mallemuck <myrtus.mallemuck@yandex.by>
2024-12-09 20:53:30 +0300

    add tests for task views

--- task/tests/test_task_views.py
+++ task/tests/test_task_views.py
@@ -1,52 +1,168 @@
+from datetime import datetime
 from django.http import Http404
 from django.shortcuts import get_object_or_404
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from task.models import Person, Category, Priority, Task
 from account.models import Profile
 from django.urls import reverse
 
 
-class PriorityCRUDTest(TestCase):
-    count_priorities = 3
+class TaskListViewTest(TestCase):
+    '''
+    Tests for task_list view (get action)
+    '''
 
-    @classmethod
-    def setUpClass(cls):
-        print('setUp')
-        test_user1 = User.objects.create_user(username='testuser1', password='pass123word')
-        test_user1.save()
-        test_user2 = User.objects.create_user(username='testuser2', password='pass123word')
-        test_user2.save()
-        Profile.objects.create(user=User.objects.get(id=1))
-        Profile.objects.create(user=User.objects.get(id=2))
-        for item in range(cls.count_priorities):
-            user_id = item % 2 + 1
-            Priority.objects.create(name=f'priority_{item}',
-                                    created_by=Person.objects.get(id=user_id))
-        cls.assertEqual(Priority.objects.all().count(), cls.count_priorities, )
+    def setUp(self):
+        self.client = Client()
+        self.test_user_info = {'username': 'testuser1',
+                               'password': 'pass123word'}
+        self.test_user1 = User.objects.create_user(username=self.test_user_info['username'],
+                                                   password=self.test_user_info['password'])
+        self.test_user1.save()
+        Profile.objects.create(user=self.test_user1)
 
-    def test_priority_not_found(self):
-        print('test_priority_not_found is run')
-        for item in range(self.count_priorities + 1):
-            try:
-                priority = get_object_or_404(Priority, name=f'priority_{item}')
-                self.assertEqual(priority.name, f'priority_{item}')
-            except Http404:
-                print(f'Priority "priority_{item}" not found.')
+    def test_task_list_authorize_user(self):
+        '''
+        for authorized user
+        '''
+        self.client.login(username=self.test_user_info['username'],
+                          password=self.test_user_info['password'])
+        response = self.client.get('/tasks/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='task/task_list.html')
 
-    def test_priority_update(self):
-        print('test_update_priority is run')
-        old_name = 'priority_1'
-        new_name = 'priority_11'
-        try:
-            priority = Priority.objects.get(name=old_name)
-            print(priority)
-            priority.name = new_name
-            priority.save()
-        except Http404:
-            print(f'Priority "{old_name}" not found before update name.')
-        try:
-            priority = Priority.objects.get(name=old_name)
-        except Http404:
-            print(f'Priority "{old_name}" not found after update name.')
+    def test_task_list_anonimous_user(self):
+        '''
+        for anonimous user (redirect to login page)
+        '''
+        response = self.client.get('/tasks/')
+        self.assertEqual(response.status_code, 302)
 
+
+class TaskDetailViewTest(TestCase):
+    '''
+    Tests for task_list view (get action)
+    '''
+
+    def setUp(self):
+        self.client = Client()
+        self.test_user_info = {'username': 'testuser1',
+                               'password': 'pass123word'}
+        self.test_user = User.objects.create_user(username=self.test_user_info['username'],
+                                                  password=self.test_user_info['password'])
+        self.test_user.save()
+        self.test_person = Person.objects.get(user=self.test_user)
+        priority = Priority.objects.create(name='testpriority', created_by=self.test_person)
+        category = Category.objects.create(name='testcategory', created_by=self.test_person, description='categoryinfo')
+        self.test_task = Task.objects.create(created_by=self.test_person,
+                                             title='test_task',
+                                             description='test_content',
+                                             completed='2024-12-09 00:00:00+03:00',
+                                             priority=priority,
+                                             category=category,
+                                             status='p')
+        self.test_task.save()
+
+    def test_task_detail_authorize_user(self):
+        '''
+        for authorized user
+        '''
+        self.client.login(username=self.test_user_info['username'],
+                          password=self.test_user_info['password'])
+        response = self.client.get(f'/tasks/{self.test_task.pk}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='task/task_detail.html')
+
+    def test_task_detail_anonimous_user(self):
+        '''
+        for anonimous user (redirect to login page)
+        '''
+        response = self.client.get(f'/tasks/{self.test_task.pk}/')
+        self.assertEqual(response.status_code, 302)
+
+
+class TaskUpdateViewTest(TestCase):
+    '''
+    Tests for task_list view (get action)
+    '''
+
+    def setUp(self):
+        self.client = Client()
+        self.test_user_info = {'username': 'testuser1',
+                               'password': 'pass123word'}
+        self.test_user = User.objects.create_user(username=self.test_user_info['username'],
+                                                  password=self.test_user_info['password'])
+        self.test_user.save()
+        self.test_person = Person.objects.get(user=self.test_user)
+        priority = Priority.objects.create(name='testpriority', created_by=self.test_person)
+        category = Category.objects.create(name='testcategory', created_by=self.test_person, description='categoryinfo')
+        self.test_task = Task.objects.create(created_by=self.test_person,
+                                             title='test_task',
+                                             description='test_content',
+                                             completed='2024-12-09 00:00:00+03:00',
+                                             priority=priority,
+                                             category=category,
+                                             status='p')
+        self.test_task.save()
+        self.url = f'/tasks/update/{self.test_task.pk}'
+
+    def test_task_update_authorize_user(self):
+        '''
+        for authorized user
+        '''
+        self.client.login(username=self.test_user_info['username'],
+                          password=self.test_user_info['password'])
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='task/task_detail.html')
+
+    def test_task_update_anonimous_user(self):
+        '''
+        for anonimous user (redirect to login page)
+        '''
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+
+
+class TaskDeleteViewTest(TestCase):
+    '''
+    Tests for task_list view (get action)
+    '''
+
+    def setUp(self):
+        self.client = Client()
+        self.test_user_info = {'username': 'testuser1',
+                               'password': 'pass123word'}
+        self.test_user = User.objects.create_user(username=self.test_user_info['username'],
+                                                  password=self.test_user_info['password'])
+        self.test_user.save()
+        self.test_person = Person.objects.get(user=self.test_user)
+        priority = Priority.objects.create(name='testpriority', created_by=self.test_person)
+        category = Category.objects.create(name='testcategory', created_by=self.test_person, description='categoryinfo')
+        self.test_task = Task.objects.create(created_by=self.test_person,
+                                             title='test_task',
+                                             description='test_content',
+                                             completed='2024-12-09 00:00:00+03:00',
+                                             priority=priority,
+                                             category=category,
+                                             status='p')
+        self.test_task.save()
+        self.url = f'/tasks/delete/{self.test_task.pk}'
+
+    def test_task_delete_authorize_user(self):
+        '''
+        for authorized user
+        '''
+        self.client.login(username=self.test_user_info['username'],
+                          password=self.test_user_info['password'])
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='task/task_delete.html')
+
+    def test_task_delete_anonimous_user(self):
+        '''
+        for anonimous user (redirect to login page)
+        '''
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
